35e1074c889db4346648ad3d08bf0fd297c5bca4
Ivan Chen <ivanchen07@gmail.com>
2024-07-24 03:04:41 -0400

    Add license headers

--- src/base64.hpp
+++ src/base64.hpp
@@ -1,3 +1,8 @@
+/*
+  implementation of base64 encoding and decoding in cpp.
+  Copyright (C) 2024  Ivan Chen
+*/
+
 #ifndef BASE64_H
 #define BASE64_H
 
--- src/main.hpp
+++ src/main.hpp
@@ -1,3 +1,8 @@
+/*
+  implementation of base64 encoding and decoding in cpp.
+  Copyright (C) 2024  Ivan Chen
+*/
+
 #ifndef MAIN_H
 #define MAIN_H
 
